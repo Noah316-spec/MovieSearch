0e4463f9e069e9a8bf87cabe81ffb2465c898bc1
Noah Nassiri <106232024+Noah316-spec@users.noreply.github.com>
2024-03-06 19:40:11 +0100

    Add files via upload

---
+++ OOP test/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace OOP_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.titel = new System.Windows.Forms.CheckBox();
+            this.year = new System.Windows.Forms.CheckBox();
+            this.genre = new System.Windows.Forms.CheckBox();
+            this.awards = new System.Windows.Forms.CheckBox();
+            this.country = new System.Windows.Forms.CheckBox();
+            this.plot = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 128);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 181);
+            this.textBox2.TabIndex = 3;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(375, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(105, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Random Movie:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Search Movie:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Random Movie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("RotisSansSerif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 33);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Movie Search";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Search Movie",
+            "Random Movie"});
+            this.comboBox2.Location = new System.Drawing.Point(183, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(105, 21);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // titel
+            // 
+            this.titel.AutoSize = true;
+            this.titel.Location = new System.Drawing.Point(267, 128);
+            this.titel.Name = "titel";
+            this.titel.Size = new System.Drawing.Size(46, 17);
+            this.titel.TabIndex = 11;
+            this.titel.Text = "Titel";
+            this.titel.UseVisualStyleBackColor = true;
+            // 
+            // year
+            // 
+            this.year.AutoSize = true;
+            this.year.Location = new System.Drawing.Point(267, 151);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(48, 17);
+            this.year.TabIndex = 12;
+            this.year.Text = "Year";
+            this.year.UseVisualStyleBackColor = true;
+            // 
+            // genre
+            // 
+            this.genre.AutoSize = true;
+            this.genre.Location = new System.Drawing.Point(267, 174);
+            this.genre.Name = "genre";
+            this.genre.Size = new System.Drawing.Size(55, 17);
+            this.genre.TabIndex = 13;
+            this.genre.Text = "Genre";
+            this.genre.UseVisualStyleBackColor = true;
+            // 
+            // awards
+            // 
+            this.awards.AutoSize = true;
+            this.awards.Location = new System.Drawing.Point(267, 197);
+            this.awards.Name = "awards";
+            this.awards.Size = new System.Drawing.Size(61, 17);
+            this.awards.TabIndex = 14;
+            this.awards.Text = "Awards";
+            this.awards.UseVisualStyleBackColor = true;
+            // 
+            // country
+            // 
+            this.country.AutoSize = true;
+            this.country.Location = new System.Drawing.Point(267, 220);
+            this.country.Name = "country";
+            this.country.Size = new System.Drawing.Size(62, 17);
+            this.country.TabIndex = 15;
+            this.country.Text = "Country";
+            this.country.UseVisualStyleBackColor = true;
+            // 
+            // plot
+            // 
+            this.plot.AutoSize = true;
+            this.plot.Location = new System.Drawing.Point(267, 243);
+            this.plot.Name = "plot";
+            this.plot.Size = new System.Drawing.Size(44, 17);
+            this.plot.TabIndex = 16;
+            this.plot.Text = "Plot";
+            this.plot.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 357);
+            this.Controls.Add(this.plot);
+            this.Controls.Add(this.country);
+            this.Controls.Add(this.awards);
+            this.Controls.Add(this.genre);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.titel);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MovieSearch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox titel;
+        private System.Windows.Forms.CheckBox year;
+        private System.Windows.Forms.CheckBox genre;
+        private System.Windows.Forms.CheckBox awards;
+        private System.Windows.Forms.CheckBox country;
+        private System.Windows.Forms.CheckBox plot;
+    }
+}
+
---
+++ OOP test/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+using System.CodeDom.Compiler;
+using System.IO;
+using static System.Windows.Forms.LinkLabel;
+
+namespace OOP_test
+{
+    public partial class Form1 : Form
+    {
+        public string ApiKey = "a514f1cf";
+        Random rnd = new Random();
+        List<string> list = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            abfrage();
+            comboboxsearch();
+        }
+        
+
+        void comboboxsearch()
+        {
+
+            int i;
+            string link, json;
+            Movie.info Info;
+           
+
+            using (WebClient Web = new WebClient())
+            {
+                do
+                {
+                    i = rnd.Next(0083658, 9900000);
+                    link = "https://www.omdbapi.com/?i=tt+" + i + "&apikey=" + ApiKey;
+                    json = Web.DownloadString(link);
+                    Info = JsonConvert.DeserializeObject<Movie.info>(json);
+                }
+                while (string.IsNullOrEmpty(Info.Title));
+
+                if (Info.Title != null)
+                {
+                    comboBox1.Items.Add(Info.Title);
+                    list.Add(Info.Title);
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i;
+            string link, json;
+            Movie.info Info;
+            textBox2.Clear();
+
+            if (list.Contains(comboBox1.Text)) 
+            {
+                using (var webClient = new WebClient())
+                {
+                    i = rnd.Next(0083658, 9900000);
+                    link = "https://www.omdbapi.com/?i=tt+" + i + "&apikey=" + ApiKey;
+                    json = webClient.DownloadString(link);
+                    Info = JsonConvert.DeserializeObject<Movie.info>(json);
+                    textBox2.Text = "Titel: " + Info.Title + Environment.NewLine + "Year: " + Info.Year + Environment.NewLine + "Genre: " + Info.Genre + Environment.NewLine + "Awards: " + Info.Awards;
+                    /*using (var web = new WebClient())
+                    {
+                        
+                        byte[] imageBytes = web.DownloadData(Info.Poster);
+
+                        using (var ms = new MemoryStream(imageBytes))
+                        {
+                            pictureBox1.Image = Image.FromStream(ms);
+                        }
+                    }*/
+                }
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == 0)
+            {
+                label1.Visible = false;
+                comboBox1.Visible = false;
+                button2.Visible = false;
+                label2.Visible = true;
+                button1.Visible = true;
+                textBox1 .Visible = true;
+
+            }
+            else if (comboBox2.SelectedIndex == 1) 
+            {
+                label1.Visible = true;
+                comboBox1.Visible = true;
+                button2.Visible = true;
+                label2.Visible = false;
+                button1.Visible = false;
+                textBox1.Visible = true;
+
+
+            }
+
+
+        }
+        void abfrage()
+        {
+            textBox2.Clear();
+
+            using (WebClient Web = new WebClient())
+            {
+
+                string link = "https://www.omdbapi.com/?t=" + textBox1.Text.Replace(' ', '+') + "&apikey=" + ApiKey;
+                var json = Web.DownloadString(link);
+                Movie.info Info = JsonConvert.DeserializeObject<Movie.info>(json);
+
+                if (titel.Checked)
+                {
+                    textBox2.Text = "Titel: " + Info.Title + Environment.NewLine;
+                }
+                if (year.Checked)
+                {
+                    textBox2.Text += "Year: " + Info.Year + Environment.NewLine;
+                }
+                if (genre.Checked)
+                {
+                    textBox2.Text+= "Genre: " + Info.Genre + Environment.NewLine;
+                }
+                if (awards.Checked)
+                {
+                    textBox2.Text+= "Award: " + Info.Awards + Environment.NewLine;
+                }
+                if (country.Checked)
+                {
+                    textBox2.Text += "Country: " + Info.Country + Environment.NewLine;
+                }
+                if (plot.Checked)
+                {
+                    textBox2.Text += "Plot: " + Info.Plot + Environment.NewLine;
+                }
+                using (var webClient = new WebClient())
+                {
+                    byte[] imageBytes = webClient.DownloadData(Info.Poster);
+
+                    using (var ms = new MemoryStream(imageBytes))
+                    {
+                        pictureBox1.Image = Image.FromStream(ms);
+                    }
+                }
+            }
+
+        }
+    }
+}
